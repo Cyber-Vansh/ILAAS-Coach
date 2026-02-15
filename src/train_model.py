77a32b8995a8ca00647921955f20a97bb063a737
Vansh Singhal <vsinghal0528@gmail.com>
2026-02-16 01:50:12 +0530

    Add model training script, EDA script, and trained model artifacts

--- src/train_model.py
+++ src/train_model.py
@@ -1,34 +1,28 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 import joblib
-from sklearn.ensemble import RandomForestClassifier
 
+df = pd.read_csv("data-cleaned/processed_student_mat.csv")
 
-
-df = pd.read_csv("../data-cleaned/processed_student_mat.csv")
 X = df.drop("risk_level", axis=1)
 y = df["risk_level"]
 
+X = pd.get_dummies(X)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-rf_model = RandomForestClassifier(
-    n_estimators=300,
-    max_depth=None,
-    random_state=42
-)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-rf_model.fit(X_train, y_train)
+model = LogisticRegression(C=1.5, max_iter=2000)
+model.fit(X_train, y_train)
 
-y_pred_rf = rf_model.predict(X_test)
+print(model.score(X_test, y_test))
 
-print("Random Forest Accuracy:", accuracy_score(y_test, y_pred_rf))
-print("\n", classification_report(y_test, y_pred_rf))
-
+joblib.dump(model, "src/student_risk_model.pkl")
+joblib.dump(scaler, "src/scaler.pkl")
+print("Model and Scaler Saved")
